2186b80403f5772bb80383b05f53f20d12d0b320
SarateanuC <cojocaru.carolina@mail.globaldatabase.com>
2023-02-08 14:28:55 +0200

    springCloud: add spring actuator

--- spring-cloud-config-client/src/main/java/com/example/springcloudconfigclient/controller/GreetingControler.java
+++ spring-cloud-config-client/src/main/java/com/example/springcloudconfigclient/controller/GreetingControler.java
@@ -3,6 +3,7 @@ package com.example.springcloudconfigclient.controller;
 import com.example.springcloudconfigclient.DbSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RefreshScope
 public class GreetingControler {
     @Value("${my.greeting: default value}")
     private String greetingMessage;
